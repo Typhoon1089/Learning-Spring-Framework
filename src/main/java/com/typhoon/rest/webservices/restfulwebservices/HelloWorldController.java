73620287a375d08c2ff88a6da7650169c4d9c004
タイフン <hungle1089@gmail.com>
2019-04-01 00:45:21 +0900

    lesson 4: Return a bean

--- src/main/java/com/typhoon/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/typhoon/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,14 +1,23 @@
 package com.typhoon.rest.webservices.restfulwebservices;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    // @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
+
+    // This function will generate a json file (not text as the previous function)
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldeBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
 }
