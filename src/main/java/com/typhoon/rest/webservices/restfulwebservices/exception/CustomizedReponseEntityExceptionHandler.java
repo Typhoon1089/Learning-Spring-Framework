8dd5b0fb1b6aa0b65a8e18f34ff81859d79db3b5
タイフン <hungle1089@gmail.com>
2019-04-06 18:43:02 +0900

    l23: create the own exception handler

---
+++ src/main/java/com/typhoon/rest/webservices/restfulwebservices/exception/CustomizedReponseEntityExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.typhoon.rest.webservices.restfulwebservices.exception;
+
+import java.util.Date;
+
+import com.typhoon.rest.webservices.restfulwebservices.user.UserNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController 
+public class CustomizedReponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptionsEntity(Exception ex, WebRequest request) throws Exception {
+        ExceptionResponse exceptionResponse = 
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+        ExceptionResponse exceptionResponse = 
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+}
