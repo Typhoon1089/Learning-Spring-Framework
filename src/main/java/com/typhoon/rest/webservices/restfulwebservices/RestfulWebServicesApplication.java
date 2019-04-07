55910fd594191c189417e6df964eba0a51018f5c
タイフン <hungle1089@gmail.com>
2019-04-08 01:34:21 +0900

    l30: property messages + HTTP header field

--- src/main/java/com/typhoon/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/typhoon/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,13 +1,38 @@
 package com.typhoon.rest.webservices.restfulwebservices;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+// import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+  // Create a bean to resolve the locate with default as US 
+  // It will be revoked when transfering param "locate" to the controller
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+  // This will handle the message, which is returned to the client
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    // This will refer the 2 files messages.properties and messages_fr.properties
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
