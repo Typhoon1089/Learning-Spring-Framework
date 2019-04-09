8e8208fe85cb268d94c537f0d1a67ceee8faa3bc
タイフン <hungle1089@gmail.com>
2019-04-09 12:07:11 +0900

    l40: Versioning in 4 ways

--- src/main/java/com/typhoon/rest/webservices/restfulwebservices/versioning/VersioningController.java
+++ src/main/java/com/typhoon/rest/webservices/restfulwebservices/versioning/VersioningController.java
@@ -11,9 +11,27 @@ public class VersioningController {
         return new PersonV1("Kaori Le");
     }
 
+    // URI versioning => Twister
     @GetMapping("v2/person")
     public PersonV2 personV2() {
         return new PersonV2(new Name("Kaori", "Le"));
     }
 
+    // Params versioning => Amazon
+    @GetMapping(value = "/person/param", params = "version=2" )
+    public PersonV2 paramV2() {
+        return new PersonV2(new Name("Kaori", "Le"));
+    }
+
+    // Header versioning => Microsoft
+    @GetMapping(value = "/person/header", headers = "X-API-VERSION=2" )
+    public PersonV2 headerV2() {
+        return new PersonV2(new Name("Kaori", "Le"));
+    }
+
+    // MIME type versioning => GitHub
+    @GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json" )
+    public PersonV2 produecesV2() {
+        return new PersonV2(new Name("Kaori", "Le"));
+    }
 }
